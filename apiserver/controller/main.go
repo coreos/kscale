450d394e9dd3fa91467dc1b165e3a644712bf944
Hongchao Deng <hongchaodeng1@gmail.com>
2016-06-10 11:05:05 -0700

    controller mem dump

--- apiserver/controller/main.go
+++ apiserver/controller/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/client/restclient"
@@ -11,13 +14,22 @@ import (
 	"k8s.io/kubernetes/pkg/util/wait"
 )
 
+const lookupCacheSizeForRC = 4096 // query controller by pod cache
+
 func main() {
-	lookupCacheSizeForRC := 4096 // query controller by pod cache
 	var apisrvAddr string
 	var concurrentRCSyncs int
+	var pprofPort int
+	var dumpdir string
 	flag.StringVar(&apisrvAddr, "addr", "localhost:8080", "APIServer addr")
+	flag.StringVar(&dumpdir, "dumpdir", "dump", "dump dir")
 	flag.IntVar(&concurrentRCSyncs, "p", 5, "Concurrent RC goroutines")
+	flag.IntVar(&pprofPort, "pprof-port", 6060, "local http handler")
 	flag.Parse()
+
+	go func() {
+		log.Println(http.ListenAndServe(fmt.Sprintf(":%d", pprofPort), nil))
+	}()
 
 	kubeconfig := &restclient.Config{
 		Host:  fmt.Sprintf("http://%s", apisrvAddr),
